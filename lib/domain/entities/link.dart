a8861f9a8ffc99e764db721eee4092a103b0d996
Marcelo dos Santos <mdssjc@gmail.com>
2023-01-01 20:54:53 -0300

    feat: add new version

--- lib/domain/entities/link.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Link {
-  IconData icon;
-  String title;
-  String url;
-
-  Link(this.icon, this.title, this.url);
-}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<h1>Bem vindo a página do MDS</h1>
+
+<div class="card p-4 mb-4">
+  <p>Nova versão da página, utilizando o SvelteKit.</p>
+  <p>Por que não estou utilizando mais o Flutter para a WEB (versão 3.0)?</p>
+  <p>O principal motivo é o baixo desempenho para páginas simples como blog.</p>
+</div>
+
+<div class="card bg-light p-4">
+  <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Notes</h5>
+          <p class="card-text">Compilação de notas sobre diversos assuntos</p>
+          <a href="https://github.com/mdssjc/mds-notes" class="btn btn-primary">ACESSAR</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Book</h5>
+          <p class="card-text">Livro de Arquitetura de Software e Testes</p>
+          <a href="https://github.com/mdssjc/mds" class="btn btn-primary">ACESSAR</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Github</h5>
+          <p class="card-text">Repositórios dos estudos e projetos</p>
+          <a href="https://github.com/mdssjc" class="btn btn-primary">ACESSAR</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Projetos</h5>
+          <ul>
+            <li><a href="https://play.google.com/store/apps/details?id=com.github.mdssjc.oblique_strategies" class="btn btn-link">Oblique Strategies</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
