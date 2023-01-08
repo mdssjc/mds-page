1d8e8c753490030e73bf93c3886c0f7293648b7c
Marcelo dos Santos <mdssjc@gmail.com>
2023-01-08 15:13:10 -0300

    feat: bump

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,27 @@
 <h1>Quem sou eu</h1>
 
-<p>
-  resumo (utilizar o texto do github)
-</p>
+<div class="card bg-light">
+  <div class="card-body">
+  <p>
+    Eu sou o Marcelo dos Santos.
+  </p>
+  <p>
+    Resolvo problemas lógicos utilizando tecnologias como Java/Kotlin, Dart, Rust e TypeScript, assim como possuo interesses em Blockchain, Programming Languages e Creative Coding.
+  </p>
+  <p>
+    Atualmente trabalho com desenvolvimento em aplicações WEB e microserviços em Java (Spring Boot) e TypeScript (Angular); e aplicações mobile em Dart (Flutter) para os projetos pessoais.
+  </p>
+  </div>
+</div>
 
+<div class="card bg-light mt-2">
+  <div class="card-body">
+    Em breve: projetos e estudos
+  </div>
+</div>
+
+<div class="card bg-light mt-2">
+  <div class="card-body">
+    Em breve: setup para desenvolvimento
+  </div>
+</div>
