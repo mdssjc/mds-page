56b0dd6171c14b99c29708015f91d50c16629208
Marcelo dos Santos <mdssjc@gmail.com>
2023-01-24 22:07:29 -0300

    fix: correct link

--- src/routes/projects/oblique-strategies/+page.svelte
+++ src/routes/projects/oblique-strategies/+page.svelte
@@ -6,7 +6,7 @@
       <strong>Oblique Strategies</strong> é um método baseado em cartão para promover a criatividade criado em conjunto pelo músico / artista <em>Brian Eno</em> e o artista multimídia <em>Peter Schmidt</em>, publicado pela primeira vez em 1975.
     </p>
     <p>
-      Instale o aplicativo para o <em>Android</em> clicando <strong><a href="https://play.google.com/store/apps/details?id=com.github.mdssjc.oblique_strategies">aqui</a></strong>!
+      <a href="https://play.google.com/store/apps/details?id=com.github.mdssjc.oblique_strategies" target="_blank" rel="noreferrer">Instale o aplicativo para o <em>Android</em>!</a>
     </p>
     <p>
       Em breve, o aplicativo será atualizando com a estrutura em <em>Riverpod</em> e <em>Material Design 3</em>.
