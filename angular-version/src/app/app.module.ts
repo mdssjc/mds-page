9fef2aa572bce87a236be60a38117715f262713f
Marcelo dos Santos <mdssjc@gmail.com>
2023-07-11 21:00:17 -0300

    feat: update styles

--- angular-version/src/app/app.module.ts
+++ angular-version/src/app/app.module.ts
@@ -14,22 +14,24 @@ import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatListModule,
-    MatMenuModule,
-    MatCardModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatDividerModule,
+        MatListModule,
+        MatMenuModule,
+        MatCardModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
