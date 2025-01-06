43f98afbec92e0b47abf69e6952fc5f02ed004a1
Marcelo dos Santos <mdssjc@gmail.com>
2025-01-05 22:15:56 -0300

    chore: add flutter version

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'MDS Page';
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: const Center(
+          child: Text('Em construção...'),
+        ),
+      ),
+    );
+  }
+}
