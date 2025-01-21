d865b27a21993bfdf6fef1c6cec4510f2c16cfa7
Marcelo dos Santos <mdssjc@gmail.com>
2025-01-20 22:39:08 -0300

    feat: add home card

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -5,16 +5,39 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
     return Container(
       padding: const EdgeInsets.all(10),
       color: Theme.of(context).colorScheme.onPrimary,
       child: GridView.count(
-        crossAxisCount: 2,
+        crossAxisCount: width >= 600 ? 2 : 1,
         children: const [
           Card(
-            child: ListTile(
-              title: Text('Home'),
-              leading: Icon(Icons.home),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    title: Text('Home'),
+                    leading: Icon(Icons.home),
+                  ),
+                  SizedBox(height: 2),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      '''Bem-vindo à nossa página!
+                  
+              Aqui, você encontrará informações detalhadas sobre nossas experiências e projetos relacionados ao desenvolvimento de software.
+              
+              Nosso objetivo é compartilhar nossas ideias e conhecimentos para ajudá-lo a entender melhor o mundo da programação e a aprimorar suas habilidades na área.
+              
+              Explore nosso conteúdo e sinta-se à vontade para entrar em contato conosco se tiver alguma dúvida ou sugestão. ''',
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Card(
