aab8d8187f03abc9d775d2669dccc608f3cc0f61
Marcelo dos Santos <mdssjc@gmail.com>
2025-01-21 22:53:46 -0300

    feat: add news card

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -41,9 +41,25 @@ class MainPage extends StatelessWidget {
             ),
           ),
           Card(
-            child: ListTile(
-              title: Text('News'),
-              leading: Icon(Icons.newspaper),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    title: Text('News'),
+                    leading: Icon(Icons.newspaper),
+                  ),
+                  SizedBox(height: 2),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      '''(##/##) Nova versão da página MDS: em Flutter.
+                      ''',
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Card(
